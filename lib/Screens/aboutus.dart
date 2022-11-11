81d6d5a6137a8df7a39f85ef62340304d4a3ead5
CompanyomTech <skylinktechnologies36@gmail.com>
2022-11-11 18:36:13 +0530

    commi

--- lib/Screens/aboutus.dart
+++ lib/Screens/aboutus.dart
@@ -62,15 +62,146 @@ class _AboutUsPageState extends State<AboutUsPage> {
                           ),
                         ],
                       ),
+                      const SizedBox(height: 10),
                       const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 10),
                         child: Text(
                             'Kurti dimond is one of the new in busy showsome in busy subribe of Kurti dimond is one of the new in busy showsome in busy subribe of '),
-                      )
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Stack(
+                            alignment: Alignment.centerLeft,
+                            children: [
+                              Image.asset(
+                                AppImages.about_bg,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(
+                                  "Our Team",
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Stack(
+                          alignment: Alignment.centerRight,
+                          children: [
+                            Container(
+                              height: Get.size.height / 9,
+                              width: Get.size.width * 0.8,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.white),
+                            ),
+                            const Align(
+                              alignment: Alignment.centerLeft,
+                              child: CircleAvatar(
+                                radius: 35,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: Get.size.width * 0.22,
+                                right: Get.size.width * 0.02,
+                              ),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: const [
+                                      Text(
+                                        "Kruti Gupta",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 5),
+                                  const Text(
+                                      "First Gen jewallry ,GHA grurad and anMBA by education kruti is a custom education kruti is a custom"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Stack(
+                          alignment: Alignment.centerRight,
+                          children: [
+                            Container(
+                              height: Get.size.height / 9,
+                              width: Get.size.width * 0.8,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Colors.white),
+                            ),
+                            const Align(
+                              alignment: Alignment.centerLeft,
+                              child: CircleAvatar(
+                                radius: 35,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: Get.size.width * 0.22,
+                                right: Get.size.width * 0.02,
+                              ),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: const [
+                                      Text(
+                                        "Kruti Gupta",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 5),
+                                  const Text(
+                                      "First Gen jewallry ,GHA grurad and anMBA by education kruti is a custom education kruti is a custom"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Stack(
+                            alignment: Alignment.centerLeft,
+                            children: [
+                              Image.asset(
+                                AppImages.about_bg,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(
+                                  "Contect Details",
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
